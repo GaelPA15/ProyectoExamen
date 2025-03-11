cb86e02cc11934ec3d69d4c821fd805fad6d9efe
Gael Velazquez <gaelvela18@gmail.com>
2025-03-11 11:56:10 -0600

    cambios

--- src/components/studenTable.tsx
+++ src/components/studenTable.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { getExamenData, updateExamenItem, deleteExamenItem } from "../lib/api";
 
 /* Acá hay un error aproposito descubrelo */
-export default function studenTable() {
+export default function StudenTable() {
   const [datos, setDatos] = useState<
     { id: number; nombre: string; completado: boolean }[]
   >([]);
